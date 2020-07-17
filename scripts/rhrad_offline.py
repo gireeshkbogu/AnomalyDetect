22ebe5785ae6df349c36e0c22f319c9d7624ddb5
Gireesh Bogu <gireeshkbogu@users.noreply.github.com>
2020-07-17 16:17:52 -0700

    Update rhrad_offline.py

--- scripts/rhrad_offline.py
+++ scripts/rhrad_offline.py
@@ -2,7 +2,7 @@
 # python rhrad_offline.py --heart_rate hr.csv --steps steps.csv
 
 # full command
-# python rhrad_offline.py --heart_rate hr.csv --steps hr.csv --myphd_id pbbqjw489c7b_offline --figure id_offine.pdf --anomalies id_offline_anomalies.csv --symptom_date 2020-01-31 --diagnosis_date 2020-01-31 --outliers_fraction 0.1 --random_seed 10 
+# python rhrad_offline.py --heart_rate hr.csv --steps hr.csv --myphd_id id_offline --figure id_offine.pdf --anomalies id_offline_anomalies.csv --symptom_date 2020-01-31 --diagnosis_date 2020-01-31 --outliers_fraction 0.1 --random_seed 10 
 # (or)
 # python rhrad_offline.py \
 #       --heart_rate hr.csv \
