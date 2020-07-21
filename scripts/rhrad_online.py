8976062d1f0c3be081a11842680fc1b0814e0d54
Gireesh Bogu <gireeshkbogu@users.noreply.github.com>
2020-07-21 15:24:02 -0700

    Update rhrad_online.py

--- scripts/rhrad_online.py
+++ scripts/rhrad_online.py
@@ -1,3 +1,9 @@
+# RHR Online Anomaly Detection
+# Author: Gireesh K. Bogu
+# Email: gbogu17@stanford.edu
+# Location: Dept.of Genetics, Stanford University
+# Date: July 18th 2020
+
 ## simple command
 # python rhrad_online.py --heart_rate hr.csv --steps steps.csv
 
