ffceaa2b271da843ea9cad7229763421997450eb
Gireesh Bogu <gireeshkbogu@users.noreply.github.com>
2020-07-21 15:24:34 -0700

    Update hrosad_online.py

--- scripts/hrosad_online.py
+++ scripts/hrosad_online.py
@@ -1,3 +1,9 @@
+# HROS Online Anomaly Detection
+# Author: Gireesh K. Bogu
+# Email: gbogu17@stanford.edu
+# Location: Dept.of Genetics, Stanford University
+# Date: July 18th 2020
+
 ## simple command
 # python hrosad_online.py --heart_rate hr.csv --steps steps.csv
 
